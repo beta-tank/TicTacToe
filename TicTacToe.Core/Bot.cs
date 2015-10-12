fca4735c9f5251c3c10c0ed84d5a5e2906d2386d
beta-tank <beta-tank@outlook.com>
2015-10-12 19:52:23 +0600

    Add bot fake.

---
+++ TicTacToe.Core/Bot.cs
@@ -0,0 +1,13 @@
+﻿using TicTacToe.Core.Enums;
+
+namespace TicTacToe.Core
+{
+    public static class Bot
+    {
+        public static bool Move(Game game, PlayerCode player)
+        {
+            byte cell = 0;
+            return game.Move(player, cell);
+        }
+    }
+}
