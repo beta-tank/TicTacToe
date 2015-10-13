1ef597afd9ea95c2bb6dfd56174b98ff1e3879c4
beta-tank <beta-tank@outlook.com>
2015-10-14 00:38:13 +0600

    Configure context.

--- TicTacToe/Data/ApplicationDbContext.cs
+++ TicTacToe/Data/ApplicationDbContext.cs
@@ -1,9 +1,35 @@
 ﻿using System.Data.Entity;
+using TicTacToe.Core;
 
 namespace TicTacToe.Web.Data
 {
     public class ApplicationDbContext : DbContext
     {
-         
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+            Database.SetInitializer<ApplicationDbContext>(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
+        }
+
+        public virtual void Commit()
+        {
+            SaveChanges();
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Field> Fields { get; set; }
+        public DbSet<Move> Moves { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Game>()
+                .HasMany<Move>(g => g.Moves)
+                .WithRequired(m => m.Game);
+            modelBuilder.Entity<Game>()
+                .HasRequired<Field>(g => g.Field)
+                .WithRequiredDependent(f => f.Game);
+
+        }
+
     }
 }
