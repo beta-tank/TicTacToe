f7f4dcbe6cb5662b8ae119ff47eaeca15e68d080
beta-tank <beta-tank@outlook.com>
2015-10-15 15:28:53 +0600

    Add field layout.

--- TicTacToe/Controllers/HomeController.cs
+++ TicTacToe/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TicTacToe.Core;
+using TicTacToe.Web.Data;
 
 namespace TicTacToe.Controllers
 {
@@ -10,11 +12,26 @@ namespace TicTacToe.Controllers
     {
         public ActionResult Index()
         {
+            var context = new ApplicationDbContext();
+            var game = new Game {Player = "123"};
+            var field = new Field() {Game = game};
+            game.Field = field;
+            context.Games.Add(game);
+            context.Commit();
+            context.Dispose();
+            return View();
+        }
+
+        public ActionResult Game()
+        {
             return View();
         }
 
         public ActionResult About()
         {
+            var context = new ApplicationDbContext();
+            var games = context.Games.ToArray();
+            context.Dispose();
             ViewBag.Message = "Your application description page.";
 
             return View();
