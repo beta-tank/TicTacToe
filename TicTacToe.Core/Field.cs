f2173cc50d35d9a6ce12f389fe1213c9775dae4d
beta-tank <beta-tank@outlook.com>
2015-10-17 10:00:34 +0600

    Commenting Field class.

--- TicTacToe.Core/Field.cs
+++ TicTacToe.Core/Field.cs
@@ -9,11 +9,19 @@ namespace TicTacToe.Core
     {
         public int Id { get; set; }
         public virtual Game Game { get; set; }
+        /// <summary>
+        /// Преобразует поле в строку  иобраьтно для хранения в БД
+        /// </summary>
         public string CellsString {
             get { return Join(";", Cells.Select(n => (byte)n));}
             set { Cells = value.Split(';').Select(n =>(PlayerCode) Convert.ToByte(n)).ToArray(); }
         }
         public PlayerCode[] Cells { get; set; }
+        /// <summary>
+        /// Индексатор для удобства обхода поля
+        /// </summary>
+        /// <param name="index">Индекс клетки</param>
+        /// <returns>Состояние клетки</returns>
         public PlayerCode this[int index] {
             get { return Cells[index]; }
             set { Cells[index] = value; }
